45111ab2db24aeacfbc1932bf39e7c78e176176c
amalhanaja <amalhanaja@gmail.com>
2020-02-20 10:54:04 +0700

    Add Poppins Font

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from './styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
+    font-family: 'Poppins', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
